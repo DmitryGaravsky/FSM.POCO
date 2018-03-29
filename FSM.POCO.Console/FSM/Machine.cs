f443e2bfad8300a9eece3505d2466c0fec40a641
DmitryGaravsky <garavskydmitry@gmail.com>
2018-03-29 23:43:21 +0300

    #api changes+sample
    #api changes+sample

---
+++ FSM.POCO.Console/FSM/Machine.cs
@@ -0,0 +1,55 @@
+﻿namespace FSM.POCO.Console {
+    using System;
+    using FSM.POCO;
+
+    // Public API part
+    public partial class Machine : IPOCOMachine<Machine.State> {
+        /*
+        {Idle} -onStart-> {Fetching}  -onSuccess-> {Idle,exit}
+                                      -onFailure-> {Error}  -onRetry-> {Idle}
+        */
+        public void Run() {
+            this.Dispatch(() => OnStart());
+        }
+    }
+    // POCO part
+    public partial class Machine : IPOCOMachine<Machine.State> {
+        protected enum State {
+            Idle,
+            Fetching,
+            Error
+        }
+        [State(State.Idle)]
+        protected void OnStart() {
+            this.SetState(State.Fetching);
+            Console.Clear();
+            Console.Write("Please, enter the integer value: ");
+            try {
+                int data = int.Parse(Console.ReadLine());
+                this.Dispatch(x => OnSuccess(x), data);
+            }
+            catch(Exception e) {
+                this.Dispatch(x => OnFailure(x), e);
+            }
+        }
+        [State(State.Fetching)]
+        protected void OnSuccess(int data) {
+            this.SetState(State.Idle);
+            Console.WriteLine("Thanks, your integer is: " + data.ToString());
+        }
+        [State(State.Fetching)]
+        protected void OnFailure(Exception e) {
+            this.SetState(State.Error);
+            Console.WriteLine("Your input incorrect:" + Environment.NewLine + e.Message);
+            this.Dispatch(() => OnRetry());
+        }
+        [State(State.Error)]
+        protected void OnRetry() {
+            this.SetState(State.Idle);
+            Console.Write("Wanna retry?");
+            var keyInfo = Console.ReadKey();
+            if(keyInfo.Key == ConsoleKey.Y)
+                this.Dispatch(() => OnStart());
+        }
+    }
+}
